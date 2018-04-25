0797fbf8e115466f5f924fa992be316719187e13
Scott Varcoe <scottv@interknowlogy.com>
2018-04-25 15:33:04 -0700

    adding swagger

--- src/todo-api/Startup.cs
+++ src/todo-api/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace todo_api
 {
@@ -23,7 +25,10 @@ namespace todo_api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen(c=>{
+                c.SwaggerDoc("v1", new Info { Title = "Todo API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,7 +38,10 @@ namespace todo_api
             {
                 app.UseDeveloperExceptionPage();
             }
-
+            app.UseSwagger();
+            app.UseSwaggerUI(cfg=>{
+                cfg.SwaggerEndpoint("/swagger/v1/swagger.json", "Todo API v1");
+            });
             app.UseMvc();
         }
     }
